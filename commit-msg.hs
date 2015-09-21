4e446917b14d350ed78ab01c5d9f01c24322167e
TJ Peden <tj.peden@tj-coding.com>
2015-09-21 10:20:28 -0500

    Refactored several functions

--- commit-msg.hs
+++ commit-msg.hs
@@ -1,13 +1,22 @@
 module Main where
 
+import Control.DeepSeq (force)
+import Control.Exception (evaluate)
+import Control.Monad (liftM)
+import Data.List (stripPrefix)
 import System.Environment (getArgs)
 import System.Exit (exitSuccess, exitFailure)
 import System.Process (readProcess)
-import Control.Monad (liftM)
-import Data.List (stripPrefix)
 
 main :: IO ()
-main = getArgs >>= parse >>= commitMsg >>= putStr
+main = getArgs >>= parse
+
+parse :: [String] -> IO ()
+parse ["-h"] = usage   >> exitSuccess
+parse ["-v"] = version >> exitSuccess
+parse []     = exitFailure
+parse args   = readFile file >>= evaluate . force >>= commitMsg >>= writeFile file
+  where file = head args
 
 commitMsg :: String -> IO String
 commitMsg text = liftM unlines $ updateFirstLine $ lines text
@@ -20,27 +29,13 @@ update :: String -> IO String
 update base = fmap ((base ++) . suffix) currentBranch
 
 suffix :: String -> String
-suffix string =
-  if match
-    then " - issue #" ++ number
-    else ""
-  where
-    (match, number) =
-      case stripPrefix "issue" string of
-        Just rest -> (True, rest)
-        Nothing -> (False, undefined)
+suffix string = maybe "" (" - issue #"++) (stripPrefix "issue" string)
 
 git :: [String] -> String -> IO String
 git = readProcess "git" -- missing parameters here
 
 currentBranch :: IO String
 currentBranch = git ["rev-parse", "--abbrev-ref", "HEAD"] ""
-
-parse :: [String] -> IO String
-parse ["-h"] = usage   >> exitSuccess
-parse ["-v"] = version >> exitSuccess
-parse []     = exitFailure
-parse args   = readFile $ head args
 
 usage :: IO ()
 usage = putStrLn "Usage: commit-msg [-vh] file"
